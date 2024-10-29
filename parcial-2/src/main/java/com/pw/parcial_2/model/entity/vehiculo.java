e10c346219aa54bc27000ea82207917d551b40fc
JonathanC <joncar3103@gmail.com>
2024-10-29 07:08:56 -0500

    Creacion de la clase vehiculo

---
+++ parcial-2/src/main/java/com/pw/parcial_2/model/entity/vehiculo.java
@@ -0,0 +1,30 @@
+package com.pw.parcial_2.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name="vehiculos")
+public class vehiculo {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="placa")
+    private String placa;
+
+    @Column(name="hora_Entrada")
+    private String hora_Entrada;
+
+    @Column(name="hora_Salida")
+    private String hora_Salida;
+
+    @Column(name="ubicacion")
+    private String ubicacion;
+}
